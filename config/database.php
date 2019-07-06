d9231b048ebbf3037895e5270e2fde0300831cb6
robbyismyid1 <robbyismyid1@gmail.com>
2019-07-06 09:47:05 +0700

    Update

--- config/database.php
+++ config/database.php
@@ -46,9 +46,9 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'https://www.db4free.net/'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'pkl_robby'),
+            'database' => env('DB_DATABASE', 'robbyismyid1'),
             'username' => env('DB_USERNAME', 'robbyismyid1'),
             'password' => env('DB_PASSWORD', 'ffx115xe52'),
             'unix_socket' => env('DB_SOCKET', ''),
